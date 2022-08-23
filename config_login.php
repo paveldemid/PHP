185e4054f77fb71e6567c222e0f1bb2b3d947ab3
paveldemid <54070482+paveldemid@users.noreply.github.com>
2022-08-23 10:49:54 +0300

    Add files via upload

---
+++ config_login.php
@@ -0,0 +1,44 @@
+<?php
+    // Получаем данные при входе в систему.
+	$date_login=array($_POST);
+     // Записываем даннные из БД.
+    $data_register=array();
+    
+    // получаем данные из JSON файла
+    $json=file_get_contents("register.json");
+    // Преобразуем в массив
+    $data_register=json_decode($json,true);
+
+    //Проверяем наличие данных пользователя в БД.
+
+   if ($date_login[0]['username']==$data_register[0]['username']) {
+
+
+       		if ($date_login[0]['password']==$data_register[0]['password']){
+
+        				include 'successful_login.html';
+        		}
+
+    			else {
+
+    		 			include 'wrong_password.html';
+    		}
+    }
+    
+
+    else {
+
+    	include 'wrong_login.html';
+    }
+    
+             
+    	 
+    	   	
+    	 
+
+            
+            
+   
+
+  
+?>
